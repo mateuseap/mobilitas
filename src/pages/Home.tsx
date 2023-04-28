55a56f4d5ee954bc6fd47c0630dbbcd6a74d80f9
Mateus Elias <meap@cin.ufpe.br>
2023-04-28 05:34:43 -0300

    feat(src/pages/Home.tsx): started using 'SemaphoresMap' component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import DefaultPage from "../components/DefaultPage/DefaultPage";
+import SemaphoresMap from "../components/SemaphoresMap/SemaphoresMap";
 
 function Home() {
-  return <DefaultPage>Home page!</DefaultPage>;
+  return (
+    <DefaultPage childrenClassName="w-full flex justify-center items-center">
+      <SemaphoresMap />
+    </DefaultPage>
+  );
 }
 
 export default Home;
