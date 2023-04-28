c5513cfd8db9e5b722667b5465fc83af08653a60
Mateus Elias <meap@cin.ufpe.br>
2023-04-28 03:52:10 -0300

    feat(src/main.tsx): rendering 'App' instead of routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import AllRoutes from "./routes";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AllRoutes />
+    <App />
   </React.StrictMode>
 );
