c20da839443740799db8fef45ce1fda0760b806c
Mateus Elias <meap@cin.ufpe.br>
2023-04-29 00:40:58 -0300

    feat(src/pages/Metrics.tsx): added 'BarChart' component

--- src/pages/Metrics.tsx
+++ src/pages/Metrics.tsx
@@ -1,7 +1,17 @@
 import DefaultPage from "../components/DefaultPage/DefaultPage";
+import BarChart from "../components/BarChart/BarChart";
 
 function Metrics() {
-  return <DefaultPage>Metrics page!</DefaultPage>;
+  const data = [10, 20, 30, 40, 50];
+  const labels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
+
+  return (
+    <DefaultPage childrenClassName="w-full flex flex-1 justify-center items-center">
+      <div className="w-[50%] text-black">
+        <BarChart data={data} labels={labels} />
+      </div>
+    </DefaultPage>
+  );
 }
 
 export default Metrics;
