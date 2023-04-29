e74a0ac615c237b3b353f83e1a8c16ee5bc408d9
Mateus Elias <meap@cin.ufpe.br>
2023-04-29 06:20:34 -0300

    feat(src/pages/Simulation.tsx): fetching solution data

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -1,7 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
 import DefaultPage from "../components/DefaultPage/DefaultPage";
 import SemaphoresMap from "../components/SemaphoresMap/SemaphoresMap";
 
 function Simulation() {
+  const fetchSolution = async () => {
+    const response = await fetch(
+      "https://plumpflickeringtab.meap0187.repl.co/solution"
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  };
+
+  const solution = useQuery({
+    retry: false,
+    queryKey: ["solution"],
+    queryFn: fetchSolution,
+  });
+
   return (
     <DefaultPage childrenClassName="w-full flex flex-1 flex-col justify-center items-center gap-y-2">
       <SemaphoresMap
@@ -11,9 +31,8 @@ function Simulation() {
         highlightIds={[33, 35, 395, 96, 682]}
         mapContainerStyle={{ width: "1280px", height: "570px" }}
       />
-      <div className="font-semibold">
-        Tempo do trajeto sem otimização: X /
-        Tempo do trajeto com a otimização: Y
+      <div className="font-medium">
+        Score do trajeto sem otimização: {solution.data['path_naive_score']} / Score do trajeto com a otimização: {solution.data['path_better_score']}
       </div>
     </DefaultPage>
   );
