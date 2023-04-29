5768c78d86281c7d098788c39c2ba02bc5293400
Mateus Elias <meap@cin.ufpe.br>
2023-04-29 05:53:34 -0300

    feat(src/pages/Simulation.tsx): added a new text section

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -3,13 +3,18 @@ import SemaphoresMap from "../components/SemaphoresMap/SemaphoresMap";
 
 function Simulation() {
   return (
-    <DefaultPage childrenClassName="w-full flex flex-1 justify-center items-center">
+    <DefaultPage childrenClassName="w-full flex flex-1 flex-col justify-center items-center gap-y-2">
       <SemaphoresMap
         simulation={true}
         center={{ lat: -8.04493, lng: -34.880372 }}
         zoom={17}
         highlightIds={[33, 35, 395, 96, 682]}
+        mapContainerStyle={{ width: "1280px", height: "570px" }}
       />
+      <div className="font-semibold">
+        Tempo do trajeto sem otimização: X /
+        Tempo do trajeto com a otimização: Y
+      </div>
     </DefaultPage>
   );
 }
