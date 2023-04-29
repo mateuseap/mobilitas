603fb5fcc0677702bd0d262e8be1adb66fa5558e
Mateus Elias <meap@cin.ufpe.br>
2023-04-29 05:38:11 -0300

    feat(src/pages/Home.tsx): modified 'SemaphoresMap' component params

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import SemaphoresMap from "../components/SemaphoresMap/SemaphoresMap";
 function Home() {
   return (
     <DefaultPage childrenClassName="w-full flex flex-1 justify-center items-center">
-        <SemaphoresMap />
+        <SemaphoresMap simulation={false} center={{ lat: -8.05428, lng: -34.8813 }} />
     </DefaultPage>
   );
 }
